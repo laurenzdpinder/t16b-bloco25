f752e800e304daa35e6f53a0fd71eadfb2858a86
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-05-10 13:21:19 -0300

    first commit heroku

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ ok: true, port: PORT });
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))

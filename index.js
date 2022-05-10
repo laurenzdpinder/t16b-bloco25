cdfa5be4365732e6d9944d1460092681a40b0166
Laurenz Pinder <laurenzdpinder90@gmail.com>
2022-05-10 15:01:12 -0300

    stage commit heroku

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 app.get('/', (req, res) => {
   res.status(200).json({ ok: true, port: PORT });
+});
+
+app.get('/env', (req, res) => {
+  res.status(200).json({ ok: true, port: NODE_ENV });
 });
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
